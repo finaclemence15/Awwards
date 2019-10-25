048a1500524b96365d8b445a0d992450c82d0917
finaclemence15 <finaclemence15@gmail.com>
2019-10-25 19:37:58 +0200

    installing boostrap

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Projects, Profile, Rating
+from .forms import NewProjectsForm
 
 # Create your views here.
+def welcome(request):
+    # profile = Profile.objects.all()
+    return render(request, 'index.html')
+
+
+@login_required(login_url='/accounts/login/')
+def add_site(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectsForm(request.POST, request.FILES)
+        if form.is_valid():
+            title = form.save(commit=False)
+            title.poster = current_user
+            title.save()
+        return redirect('welcome')
+
+    else:
+        form = NewProjectsForm()
+    return render(request, 'create_site.html', {"form": form})
