4fda20a8e2a2bd46ad799769d1cd79631e720106
finaclemence15 <finaclemence15@gmail.com>
2019-10-26 10:32:35 +0200

    adding template

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -4,4 +4,4 @@ from .models import Projects
 class NewProjectsForm(forms.ModelForm):
     class Meta:
         model = Projects
-        exclude = ['profile', 'post_date']
+        exclude = ['profile', 'post_date', 'poster']
