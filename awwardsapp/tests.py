3a5fa35114b2f03bba18aa49a40852ebcde9b5db
finaclemence15 <finaclemence15@gmail.com>
2019-10-27 01:55:56 +0200

    set up test for Profile model

--- awwardsapp/tests.py
+++ awwardsapp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from . models import Projects, Profile, Rating
 
 # Create your tests here.
+
+# Profile model test  
+      
+class ProfileTestClass(TestCase):        
+    
+        # Set up method
+    def setUp(self):
+        self.image= Profile(profile_pict = 'img.jpg', bio ='image')
+        # self.images= Profile(profile1 = 'piza1.jpg', bio ='pizza')
+        
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Profile))     
