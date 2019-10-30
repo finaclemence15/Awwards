8f149012f937e0d43d033451cf0824dbda30ce1b
finaclemence15 <finaclemence15@gmail.com>
2019-10-30 18:09:46 +0200

    adding a link of project

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -4,7 +4,7 @@ from .models import Projects,Profile
 class NewProjectsForm(forms.ModelForm):
     class Meta:
         model = Projects
-        exclude = ['profile', 'post_date', 'poster']
+        exclude = ['profile', 'post_date', 'poster', 'description']
         
 class NewProfileForm(forms.ModelForm):
     class Meta:
